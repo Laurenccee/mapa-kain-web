8e37e3f1e0f5a328f670544e9a8def869c8aeb9b
Revert Laurence Tejada <tejadarevertlaurence@gmail.com>
2026-05-18 20:17:13 +0800

    refactor(BackButton): simplify goBack function for improved readability

--- src/components/shared/BackButton.tsx
+++ src/components/shared/BackButton.tsx
@@ -7,13 +7,15 @@ import { useRouter } from 'next/navigation';
 
 export default function BackButton() {
   const router = useRouter();
+  const goBack = () => {
+    if (window.history.length > 1) {
+      router.back();
+    } else {
+      router.replace('/map');
+    }
+  };
   return (
-    <Button
-      variant="secondary"
-      size="icon-lg"
-      onClick={() => router.back()}
-      asChild
-    >
+    <Button variant="secondary" size="icon-lg" onClick={goBack} asChild>
       <HugeiconsIcon icon={ArrowLeft01Icon} />
     </Button>
   );
